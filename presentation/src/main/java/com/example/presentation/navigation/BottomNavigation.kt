9407230a06c2aed58e94310196aa8ecd1735b5ba
jeongwoobin <woobin406@naver.com>
2024-11-15 09:59:27 +0900

    [feat] adding MyBottomNavigation

--- presentation/src/main/java/com/example/presentation/navigation/BottomNavigation.kt
+++ presentation/src/main/java/com/example/presentation/navigation/BottomNavigation.kt
@@ -3,6 +3,15 @@ package com.example.presentation.navigation
 import androidx.annotation.DrawableRes
 import androidx.annotation.IdRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 sealed class MenuItem(
     // Resource ID of the icon for the menu item
@@ -30,4 +39,60 @@ sealed class MenuItem(
         R.string.settings,
         R.id.SettingsFragment
     )
+}
+
+@Composable
+private fun MyBottomNavigation(
+    modifier: Modifier = Modifier,
+    containerColor: Color,
+    contentColor: Color,
+    indicatorColor: Color,
+    navController: NavHostController
+) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Rating,
+        BottomNavItem.Profile
+    )
+
+    AnimatedVisibility(
+        visible = items.map { it.screenRoute }.contains(currentRoute)
+    ) {
+        NavigationBar(
+            modifier = modifier,
+            containerColor = containerColor,
+            contentColor = contentColor,
+        ) {
+            items.forEach { item ->
+                NavigationBarItem(
+                    selected = currentRoute == item.screenRoute,
+                    label = {
+                        Text(
+                            text = stringResource(id = item.title),
+                            style = TextStyle(
+                                fontSize = 12.sp
+                            )
+                        )
+                    },
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = item.icon),
+                            contentDescription = stringResource(id = item.title)
+                        )
+                    },
+                    onClick = {
+                        navController.navigate(item.screenRoute) {
+                            navController.graph.startDestinationRoute?.let {
+                                popUpTo(it) { saveState = true }
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                )
+            }
+        }
+    }
 }
