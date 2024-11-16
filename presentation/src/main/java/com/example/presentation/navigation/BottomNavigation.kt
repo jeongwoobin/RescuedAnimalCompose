7113857c20c45c57d073d872d3953c198fc68ce2
jeongwoobin <woobin406@naver.com>
2024-11-16 22:59:42 +0900

    [feat] add import

--- presentation/src/main/java/com/example/presentation/navigation/BottomNavigation.kt
+++ presentation/src/main/java/com/example/presentation/navigation/BottomNavigation.kt
@@ -6,6 +6,7 @@ import androidx.annotation.StringRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
