794c426627ac318ff0b503afd772440cf8946c2a
jeongwoobin <woobin406@naver.com>
2024-11-20 16:04:22 +0900

    [feat] ing nestedNavigation graph

--- presentation/src/main/java/com/example/presentation/navigation/MainNavGraph.kt
+++ presentation/src/main/java/com/example/presentation/navigation/MainNavGraph.kt
@@ -3,6 +3,7 @@ package com.example.presentation.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.navigation
 import com.example.presentation.screens.favoriteScreen.favoriteScreenRoute
 import com.example.presentation.screens.myPage.myPageScreenRoute
 import com.example.presentation.screens.rescuedAnimalScreen.rescuedAnimalScreenRoute
@@ -12,6 +13,25 @@ fun MainNavGraph(navController: NavHostController, startDestination: String) {
 
     NavHost(navController = navController, startDestination = startDestination) {
 
+        navigation(
+            startDestination = Screen.RescuedAnimalScreen.route,
+            route = AuthScreen.Auth.route
+        ) {
+            composable(AuthScreen.Login.route) {
+                LoginScreen(
+                    loginAction = {
+                        navController.navigate(MainScreen.Home.route) {
+                            popUpTo(AuthScreen.Auth.route) {
+                                inclusive = true
+                            }
+                        }
+                    }
+                )
+            }
+            composable(AuthScreen.SignUp.route) {
+                SignUpScreen()
+            }
+        }
         // RescuedAnimalScreen
         rescuedAnimalScreenRoute(navController = navController)
 
